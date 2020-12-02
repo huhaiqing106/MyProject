30d0438891ab733da68d16cfc5db1bbc32593881
huhaiqing <huhaiqing106@sina.com>
2020-12-02 15:09:05 +0800

    提交react项目模板

---
+++ DemoProject/react-temlpate/babel.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('@babel/core').TransformOptions}
+ */
+module.exports = {
+	presets: [
+		[
+			'@babel/env',
+			{
+				useBuiltIns: 'usage',
+				corejs: 3,
+			},
+		],
+		'@babel/preset-react',
+		'@babel/preset-typescript',
+	],
+};
---
+++ DemoProject/react-temlpate/src/index.tsx
@@ -0,0 +1,5 @@
+import App from './App';
+import ReactDom from 'react-dom';
+import React from 'react';
+
+ReactDom.render(<App />, document.querySelector('#root'));
