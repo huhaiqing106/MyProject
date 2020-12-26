9f93d314f975eb7190a9dcdf83ebe0d8284a6f70
Phil <39490885+huhaiqing106@users.noreply.github.com>
2020-12-26 15:55:55 +0800

    Update work-cli-svn.js

--- DemoCli/bin/work-cli-svn.js
+++ DemoCli/bin/work-cli-svn.js
@@ -1,10 +1,10 @@
 const svnUltiMate = require('node-svn-ultimate');
 
 module.exports = (projectName) => {
-  const svnUrl = 'http://192.168.20.8/svn/TS_Product/JavaPlatForm/jreap/08_工程模板及示例/02branches/0.2.S1/jreap-pro/';
+  const svnUrl = 'xxxx';
 
   return new Promise((resolve, reject) => {
-    svnUltiMate.commands.checkout(svnUrl, projectName, { username: 'huhq', password: 'Huhq#909152' }, function (err) {
+    svnUltiMate.commands.checkout(svnUrl, projectName, { username: 'xxx', password: 'xxx' }, function (err) {
       resolve(err);
     });
   });
